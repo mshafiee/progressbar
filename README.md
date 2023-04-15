70fb94b4ddaa836a7c7a363e51cd6a236598d5a1
Mohammad Shafiee <muhammad.shafiee@gmail.com>
2023-04-15 16:31:31 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,67 @@
+ProgressBar Package
+===================
+
+This package provides several types of progress bars that can be used in command-line interfaces.
+
+Usage
+-----
+
+Import the package with:
+
+go
+
+```go
+import "github.com/mshafiee/progressbar"
+```
+
+Call any of the functions in the package to display a progress bar.
+
+For example, to display a simple progress bar:
+
+go
+
+```go
+progressbar.SimpleProgressBar(50, 100)
+```
+
+This will display a progress bar indicating 50% completion.
+
+Available Progress Bars
+-----------------------
+
+The package currently provides the following progress bars:
+
+*   SimpleProgressBar
+*   ProgressBarWithPercentage
+*   ArrowProgressBar
+*   ColorArrowProgressBar
+*   BlockProgressBar
+*   ColorBlockProgressBar
+*   RotatingProgressBar
+*   AnimatedArrowProgressBar
+
+Each function takes two arguments: `completed` and `total`. These are the current progress and the total progress, respectively. They should be integers.
+
+Example
+-------
+
+go
+
+```go
+package main
+
+import (
+	"github.com/mshafiee/progressbar"
+	"time"
+)
+
+func main() {
+	total := 100
+	for i := 0; i <= total; i++ {
+		progressbar.ArrowProgressBar(i, total)
+		time.Sleep(50 * time.Millisecond)
+	}
+}
+```
+
+This will display an arrow progress bar that fills up as the loop progresses.
---
+++ progressbar.go
@@ -0,0 +1,115 @@
+package progressBar
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+const (
+	progressWidth = 50
+	progressChars = `|/-\`
+)
+
+func ColorArrowProgressBar(completed, total int) {
+	progress := float64(completed) / float64(total)
+	blocks := int(progress * progressWidth)
+
+	color := "\033[1;31m"
+	if progress >= 0.5 {
+		color = "\033[1;33m"
+	}
+	if progress >= 0.9 {
+		color = "\033[1;32m"
+	}
+
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("[%s", color))
+	sb.WriteString(strings.Repeat("=", blocks))
+	if progressWidth-blocks != 0 {
+		sb.WriteString(">")
+	} else {
+		sb.WriteString("=")
+	}
+	sb.WriteString("\u001B[0m")
+	sb.WriteString(strings.Repeat(".", progressWidth-blocks))
+	sb.WriteString(fmt.Sprintf("] %.2f%%", progress*100))
+	fmt.Printf("\r%s\033[0m", sb.String())
+}
+
+func RotatingProgressBar(completed, total int) {
+	progress := float64(completed) / float64(total)
+	blocks := int(progress * progressWidth)
+
+	index := (completed / 2) % len(progressChars)
+	char := progressChars[index : index+1]
+
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("[%s", strings.Repeat("=", blocks)))
+	sb.WriteString(char)
+	sb.WriteString(strings.Repeat(".", progressWidth-blocks))
+	sb.WriteString(fmt.Sprintf("] %.2f%%", progress*100))
+	fmt.Printf("\r%s", sb.String())
+	time.Sleep(100 * time.Millisecond)
+}
+
+func BlockProgressBar(completed, total int) {
+	progress := float64(completed) / float64(total)
+	blocks := int(progress * progressWidth)
+
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("[%s%s] %.2f%%", strings.Repeat("█", blocks), strings.Repeat("░", progressWidth-blocks), progress*100))
+	fmt.Printf("\r%s", sb.String())
+}
+
+func ArrowProgressBar(completed, total int) {
+	progress := float64(completed) / float64(total)
+	blocks := int(progress * progressWidth)
+
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("[%s>%s] %.2f%%", strings.Repeat("=", blocks), strings.Repeat(".", progressWidth-blocks), progress*100))
+	fmt.Printf("\r%s", sb.String())
+}
+
+func ColorBlockProgressBar(completed, total int) {
+	progress := float64(completed) / float64(total)
+	blocks := int(progress * progressWidth)
+
+	var sb strings.Builder
+	sb.WriteString("\r[")
+	sb.WriteString(strings.Repeat("\033[32m█\033[0m", blocks))
+	sb.WriteString(strings.Repeat("\033[31m░\033[0m", progressWidth-blocks))
+	sb.WriteString(fmt.Sprintf("] %.2f%%", progress*100))
+	fmt.Printf("%s", sb.String())
+}
+
+func AnimatedArrowProgressBar(completed, total int) {
+	progress := float64(completed) / float64(total)
+	blocks := int(progress * progressWidth)
+
+	index := int(time.Now().UnixNano()/100000000) % len(progressChars)
+	char := progressChars[index : index+1]
+
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("[%s%s>%s] %.2f%%", strings.Repeat("=", blocks), char, strings.Repeat(".", progressWidth-blocks), progress*100))
+	fmt.Printf("\r%s", sb.String())
+}
+
+func SimpleProgressBar(completed, total int) {
+	progress := float64(completed) / float64(total)
+	blocks := int(progress * progressWidth)
+
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("[%s%s] %.2f%%", strings.Repeat("=", blocks), strings.Repeat(" ", progressWidth-blocks), progress*100))
+	fmt.Printf("\r%s", sb.String())
+}
+
+func ProgressBarWithPercentage(completed, total int) {
+	progress := float64(completed) / float64(total)
+	blocks := int(progress * progressWidth)
+
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("[%s%s] %.2f%% ", strings.Repeat("=", blocks), strings.Repeat(" ", progressWidth-blocks), progress*100))
+	sb.WriteString(fmt.Sprintf("  %d/%d", completed, total))
+	fmt.Printf("\r%s", sb.String())
+}
